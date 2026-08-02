ecb9364ac96f664fc6b9f30f94dd43e6e631b0e3
Daryaastudio <Ahmad.barakzaii90@gmail.com>
2026-08-02 19:34:43 -0400

    Add menu toggle functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// Daryaa Studios JavaScript
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
